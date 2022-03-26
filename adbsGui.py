8827cf494fa48ee43eebea27f1a30a04c656ba3e
Pragyan Shrestha <pragyanOne@gmail.com>
2022-03-26 17:33:17 +0545

    updated adbsGui.py

--- adbsGui.py
+++ adbsGui.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from adbs import ad2bs, bs2ad
+from datetime import datetime
 
 
 def bs2adM(date):
@@ -31,9 +32,11 @@ adFrame.grid(row=1, column=1)
 tk.Label(bsFrame, text="BS date").grid()
 tk.Label(adFrame, text="AD date").grid()
 bsDateEntry = tk.Entry(bsFrame, textvariable=bsDate)
+bsDateEntry.insert(0, ad2bs(datetime.now().strftime("%Y-%m-%d")))
 bsDateEntry.grid(padx=2)
 bsDateEntry.bind("<Return>", lambda _: bs2adM(bsDate.get()))
 adDateEntry = tk.Entry(adFrame, textvariable=adDate)
+adDateEntry.insert(0, datetime.now().strftime("%Y-%m-%d"))
 adDateEntry.grid(padx=2)
 adDateEntry.bind("<Return>", lambda _: ad2bsM(adDate.get()))
 tk.Button(bsFrame, text="Convert to AD", command=lambda: bs2adM(bsDate.get())).grid(
